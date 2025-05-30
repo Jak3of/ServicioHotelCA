aefb41e241fc205a0acd9c9d6d520bf0b6aeac34
LAB-USR-LSUR <LAB-USR-LSUR@grupoutp.net>
2025-05-29 21:16:21 -0500

    first commit

---
+++ src/main/java/com/mycompany/avanceproyecto/AvanceProyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.avanceproyecto;
+
+/**
+ *
+ * @author LAB-USR-LSUR
+ */
+public class AvanceProyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
